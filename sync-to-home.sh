8ddb7ff5116d23605f95e227ac88b2baa96d330a
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-06-13 18:11:28 -0700

    remove gitignore

--- sync-to-home.sh
+++ sync-to-home.sh
@@ -3,12 +3,24 @@ mkdir -p ~/.config/alacrity ~/.config/lvim ~/.config/lvim/ftdetect ~/.config/lvi
 
 # printf "if [ -f ~/.bash_aliases ]; then\n\t. ~/.bash_aliases\nfi\n" >> ~/.bashrc
 
-for f in \
-	.Rprofile .bash_aliases .condarc .inputrc .tmux.conf .vimrc \
-	config/alacritty/alacritty.yml \
-	config/lvim/config.lua config/lvim/ftdetect/snakemake.vim config/lvim/syntax/snakemake.vim \
-	config/starship.toml
+for f in .Rprofile .bash_aliases .condarc .inputrc .tmux.conf .vimrc 
 do
-	ln -s $@ `realpath $f` ~/.
+	SRC=`realpath ${f}`
+	DEST="${HOME}/."
+	echo $SRC $DEST
+	ln -s $@ $SRC $DEST
 done
 
+
+for f in \
+	config/alacritty/alacritty.yml \
+	config/lvim/config.lua \
+	config/lvim/ftdetect/snakemake.vim \
+	config/lvim/syntax/snakemake.vim \
+	config/starship.toml 
+do
+	SRC=`realpath ${f}`
+	DEST="${HOME}/.${f}"
+	echo $SRC $DEST
+	ln -s $@ $SRC $DEST
+done
