cb65e536afceae10ec05235e83483afef56f2b50
Mitchell R. Vollger <mvollger@gmai.com>
2024-11-12 09:29:58 -0800

    .

--- sync-to-home.sh
+++ sync-to-home.sh
@@ -12,6 +12,7 @@ set -euo pipefail
 
 git pull
 mkdir -p ~/.config/alacritty ~/.config/lvim ~/.config/lvim/ftdetect ~/.config/lvim/syntax
+mkdir -p ~/bin
 
 # printf "if [ -f ~/.bash_aliases ]; then\n\t. ~/.bash_aliases\nfi\n" >> ~/.bashrc
 
@@ -23,6 +24,7 @@ for f in .Rprofile \
     .tmux.conf \
     .vimrc \
     .gitconfig \
+    bin/start.sh \
     .config/alacritty/alacritty.yml \
     .config/lvim/config.lua \
     .config/lvim/ftdetect/snakemake.vim \
